2381ca3b987e49bc854ace872dc3f2212bcdef0d
Kenshi <diegocabe87@gmail.com>
2022-08-20 22:24:36 -0300

    Se agrego Estilos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import {Contador} from "./Contador"
 
 function App() {
+
+  const onAdd = (contador) => {
+    console.log ('Soy onAdd y el valor del contador es:', contador)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Contador onAdd={onAdd}/>
     </div>
   );
 }
